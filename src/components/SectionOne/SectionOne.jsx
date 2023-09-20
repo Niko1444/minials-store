ddd571d437cb60849cb51b6eb961726d4d281c38
Le Tien Phat <91601109+Niko1444@users.noreply.github.com>
2023-09-20 14:16:13 +0700

    [SectionOne] responsive okay with larger than 992px devices

--- src/components/SectionOne/SectionOne.jsx
+++ src/components/SectionOne/SectionOne.jsx
@@ -7,28 +7,7 @@ const SectionOne = () => {
   return (
     <div className="SectionOne">
       <div className="SectionOne__Carousel">
-        <Carousel>
-          {/* <Carousel.Item interval={10000000}>
-            <img
-              src="./public/images/minimal_shop_1.jpg"
-              alt="minimal shop image 1"
-              className="Carousel__Image"
-            />
-          </Carousel.Item>
-          <Carousel.Item interval={1000}>
-            <img
-              src="./public/images/minimal_shop_2.jpg"
-              alt="minimal shop image 2"
-              className="Carousel__Image"
-            />
-          </Carousel.Item>
-          <Carousel.Item interval={1000}>
-            <img
-              src="./public/images/minimal_shop_3.jpg"
-              alt="minimal shop image 3"
-              className="Carousel__Image"
-            />
-          </Carousel.Item> */}
+        <Carousel controls={false}>
           {JSON.map((item, id) => {
             return (
               <Carousel.Item interval={100000} key={id}>
@@ -37,13 +16,22 @@ const SectionOne = () => {
             );
           })}
         </Carousel>
-        <div className="SectionOne__Cover">
-          <div className="SectionOne__Cover--Title">
-            <h1>just minimalism</h1>
+      </div>
+      <div className="SectionOne__Cover">
+        <div className="SectionOne__Cover--Title">
+          <h1>just minimalism</h1>
+        </div>
+        <div className="SectionOne__Cover--Description">
+          <div className="TextBox">
+            <h5>
+              "Kindness, patience, and grace. <br /> The extraordinary found in
+              the ordinary". <br /> Bringing you not the most luxurious, but the
+              feeling of comfort and worthy.
+            </h5>
           </div>
-          <div className="SectionOne__Cover--Button">
-            <Button>Shop now</Button>
-          </div>
+        </div>
+        <div className="SectionOne__Cover--Button">
+          <Button>Shop now</Button>
         </div>
       </div>
     </div>
